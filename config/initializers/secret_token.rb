86eabdea9e952e58aed41d6c5918b703e251949a
Tom Darci <tom.darci@opower.com>
2012-11-06 14:53:20 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bonkers::Application.config.secret_token = '3953818f2e8686b2ae84aeb858bed7e70f23441a4a8a0813c3f9402a03404f91bea45354b3b89add05f8c72a520f65d482bb0656e2a2caec6bf004c95a78cbe9'
